607f75080325bfb39376313e5ffc8ec812c147d1
kunal jain <kunal.jain21994@gmail.com>
2017-03-19 01:17:48 +0530

    sent to email

--- app/src/main/java/com/example/kunal/justjava/MainActivity.java
+++ app/src/main/java/com/example/kunal/justjava/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.kunal.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -27,10 +30,16 @@ public class MainActivity extends AppCompatActivity {
        kj.setText(""+(Integer.parseInt(kj.getText().toString())+1));
     }
     public void decrement(View v){
+        int qunat;
         TextView kj=(TextView)findViewById(R.id.quantity_textx_View);
-        
+        qunat=Integer.parseInt(kj.getText().toString());
         //kj.setText(Integer.parseInt(kj.getText().toString())-1);
-        kj.setText(""+(Integer.parseInt(kj.getText().toString())-1));
+        if (qunat<0||qunat==0){
+            Toast.makeText(getApplicationContext(),"No. less than 0",Toast.LENGTH_LONG).show();
+        }
+        else
+        kj.setText(""+(qunat-1));
+
     }
 
     public void displayPrice(int k){
@@ -42,6 +51,16 @@ public class MainActivity extends AppCompatActivity {
         if(cbox.isChecked()){
             crm="Whipped Cream added";
         }
-        kj.setText(NumberFormat.getCurrencyInstance().format(k)+"\n"+crm);
+        {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_SUBJECT,"JAva app subject" );
+//            System.out.println("subject added");
+            intent.putExtra(Intent.EXTRA_TEXT,"this is the body of the email\n\t"+NumberFormat.getCurrencyInstance().format(k)+"\n\t"+crm);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+//        kj.setText(NumberFormat.getCurrencyInstance().format(k)+"\n"+crm);
     }
 }
